f1936b19fc07d8645821343766a8cb27a5874656
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-06-21 19:52:35 -0600

    Actions: Move cmake configure to build.yml for MSVC

--- .ci/windows-msvc.sh
+++ .ci/windows-msvc.sh
@@ -1,21 +1,6 @@
 #!/bin/sh -ex
 
-mkdir build && cd build
-
-cmake .. -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-    -DYUZU_USE_BUNDLED_VCPKG=OFF \
-    -DYUZU_TESTS=OFF \
-    -DYUZU_ENABLE_LTO=ON \
-    -DYUZU_USE_BUNDLED_QT=OFF \
-    -DYUZU_USE_BUNDLED_SDL2=OFF \
-    -DYUZU_USE_EXTERNAL_SDL2=ON \
-    -DUSE_DISCORD_PRESENCE=ON \
-    -DYUZU_USE_QT_WEB_ENGINE=ON \
-    -DUSE_SYSTEM_ZSTD=OFF \
-    -DENABLE_WEB_SERVICE=OFF
+cd build
 
 ninja
 ninja bundle

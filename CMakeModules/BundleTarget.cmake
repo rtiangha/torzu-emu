abab9fef67b15018a6083c0b2f857702ee64c5e1
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-06-21 18:03:18 -0600

    Actions: Switch to QT6 for all desktop builds

--- CMakeModules/BundleTarget.cmake
+++ CMakeModules/BundleTarget.cmake
@@ -32,8 +32,8 @@ if (BUNDLE_TARGET_EXECUTE)
             # This ensures Qt can find its plugins.
             file(WRITE "${executable_parent_dir}/qt.conf" "[Paths]\nPrefix = .")
 
-            find_program(windeployqt_executable windeployqt PATHS "${QT_HOST_PATH}/bin")
-            find_program(qtpaths_executable qtpaths PATHS "${QT_HOST_PATH}/bin")
+            find_program(windeployqt_executable windeployqt6 PATHS "${QT_HOST_PATH}/bin")
+            find_program(qtpaths_executable qtpaths6 PATHS "${QT_HOST_PATH}/bin")
 
             # TODO: Hack around windeployqt's poor cross-compilation support by
             # TODO: making a local copy with a prefix pointing to the target Qt.
@@ -42,23 +42,23 @@ if (BUNDLE_TARGET_EXECUTE)
                 file(MAKE_DIRECTORY "${windeployqt_dir}")
                 symlink_safe_copy("${windeployqt_executable}" "${windeployqt_dir}/windeployqt.exe")
                 symlink_safe_copy("${qtpaths_executable}" "${windeployqt_dir}/qtpaths.exe")
-                symlink_safe_copy("${QT_HOST_PATH}/bin/Qt5Core.dll" "${windeployqt_dir}")
+                symlink_safe_copy("${QT_HOST_PATH}/bin/Qt6Core.dll" "${windeployqt_dir}")
 
                 if (EXISTS "${QT_TARGET_PATH}/share")
                     # Unix-style Qt; we need to wire up the paths manually.
                     file(WRITE "${windeployqt_dir}/qt.conf" "\
                         [Paths]\n
                         Prefix = ${QT_TARGET_PATH}\n \
-                        ArchData = ${QT_TARGET_PATH}/share/qt5\n \
+                        ArchData = ${QT_TARGET_PATH}/share/qt6\n \
                         Binaries = ${QT_TARGET_PATH}/bin\n \
-                        Data = ${QT_TARGET_PATH}/share/qt5\n \
-                        Documentation = ${QT_TARGET_PATH}/share/qt5/doc\n \
-                        Headers = ${QT_TARGET_PATH}/include/qt5\n \
+                        Data = ${QT_TARGET_PATH}/share/qt6\n \
+                        Documentation = ${QT_TARGET_PATH}/share/qt6/doc\n \
+                        Headers = ${QT_TARGET_PATH}/include/qt6\n \
                         Libraries = ${QT_TARGET_PATH}/lib\n \
-                        LibraryExecutables = ${QT_TARGET_PATH}/share/qt5/bin\n \
-                        Plugins = ${QT_TARGET_PATH}/share/qt5/plugins\n \
-                        QmlImports = ${QT_TARGET_PATH}/share/qt5/qml\n \
-                        Translations = ${QT_TARGET_PATH}/share/qt5/translations\n \
+                        LibraryExecutables = ${QT_TARGET_PATH}/share/qt6/bin\n \
+                        Plugins = ${QT_TARGET_PATH}/share/qt6/plugins\n \
+                        QmlImports = ${QT_TARGET_PATH}/share/qt6/qml\n \
+                        Translations = ${QT_TARGET_PATH}/share/qt6/translations\n \
                     ")
                 else()
                     # Windows-style Qt; the defaults should suffice.
@@ -71,7 +71,7 @@ if (BUNDLE_TARGET_EXECUTE)
 
             message(STATUS "Executing windeployqt for executable ${executable_path}")
             execute_process(COMMAND "${windeployqt_executable}" "${executable_path}"
-#                --qtpaths "${qtpaths_executable}"
+                --qtpaths "${qtpaths_executable}"
                 --no-compiler-runtime --no-system-d3d-compiler --no-opengl-sw --no-translations
                 --plugindir "${executable_parent_dir}/plugins"
                 RESULT_VARIABLE windeployqt_result)
@@ -115,7 +115,7 @@ if (BUNDLE_TARGET_EXECUTE)
 
         if (enable_qt)
             # Find qmake to make sure the plugin uses the right version of Qt.
-            find_program(qmake_executable qmake PATHS "${QT_HOST_PATH}/bin")
+            find_program(qmake_executable qmake6 PATHS "${QT_HOST_PATH}/bin")
 
             set(extra_linuxdeploy_env "QMAKE=${qmake_executable}")
             set(extra_linuxdeploy_args --plugin qt)

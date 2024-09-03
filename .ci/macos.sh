7672f4c05ce39ccfb6e68d512ffb319ce14ae938
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-09-03 09:17:28 -0600

    workflows: Freeze boost on MacOS at 1.85.0

--- .ci/macos.sh
+++ .ci/macos.sh
@@ -1,21 +1,7 @@
 #!/bin/bash -ex
 
-mkdir build && cd build
-cmake .. -GNinja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_OSX_ARCHITECTURES="$TARGET" \
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_CXX_FLAGS="-O2" \
-    -DCMAKE_C_FLAGS="-O2" \
-    -DTORZU_ENABLE_LTO=ON \
-    -DTORZU_USE_BUNDLED_VCPKG=OFF \
-    -DTORZU_USE_QT_WEB_ENGINE=ON \
-    -DTORZU_TESTS=OFF \
-    -DENABLE_WEB_SERVICE=OFF \
-    -DENABLE_LIBUSB=OFF \
-    -DENABLE_QT6=ON \
-    -DUSE_DISCORD_PRESENCE=ON
+cd build
+
 ninja
 ninja bundle
 

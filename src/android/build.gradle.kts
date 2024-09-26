dc74409b4cfe9a4cf0ffa8183dd5a84330f94ee3
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-09-26 09:14:30 -0600

    Android: Update various dependencies

--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -3,8 +3,8 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
+    id("com.android.application") version "8.6.1" apply false
+    id("com.android.library") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
 
@@ -17,6 +17,6 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.1")
     }
 }

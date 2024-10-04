d29d404b3bd94e0fe495c125d2bbf4285c288083
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-10-04 13:54:05 -0600

    ci: Switch to Mold linker on AppImage runner

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
 
 if [ "$TARGET" = "appimage" ]; then
-    # Compile the AppImage we distribute with Clang.
-    export EXTRA_CMAKE_FLAGS=(-DCMAKE_LINKER=/usr/bin/lld-17)
     # Bundle required QT wayland libraries
     export EXTRA_QT_PLUGINS="waylandcompositor"
     export EXTRA_PLATFORM_PLUGINS="libqwayland-egl.so;libqwayland-generic.so"

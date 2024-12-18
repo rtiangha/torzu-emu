340c9a66b39e2f4412b50977e7ca9df40c2575a0
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-12-17 23:03:35 -0700

    Android: cmake 3.30.6

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -152,7 +152,7 @@ android {
 
     externalNativeBuild {
         cmake {
-            version = "3.22.1"
+            version = "3.30.6"
             path = file("../../../CMakeLists.txt")
         }
     }

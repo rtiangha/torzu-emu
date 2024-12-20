1214cd06e42a1f49d5ce276e47e4f7ca92950972
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-12-19 17:09:22 -0700

    externals: Vulkan-ValidationLayers c552e55

--- .ci/scripts/android/build.sh
+++ .ci/scripts/android/build.sh
@@ -18,6 +18,45 @@ if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
     base64 --decode <<< "${ANDROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
 fi
 
+# Build Vulkan-ValidationLayers
+mkdir -p src/android/app/build/tmp
+mkdir -p assets/libs/lib/arm64-v8a
+
+cd externals/Vulkan-ValidationLayers
+
+# Build vvl release binary for arm64-v8a
+cmake -S . -B build \
+  -D CMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
+  -D CMAKE_C_COMPILER_LAUNCHER=ccache \
+  -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
+  -D CMAKE_CXX_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android30-clang++ \
+  -D CMAKE_C_COMPILER=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android30-clang \
+  -D CMAKE_CXX_FLAGS="-O2" \
+  -D CMAKE_C_FLAGS="-O2" \
+  -D CMAKE_EXE_LINKER_FLAGS=-flto=thin \
+  -D CMAKE_SHARED_LINKER_FLAGS=-flto=thin \
+  -D ANDROID_PLATFORM=30 \
+  -D CMAKE_ANDROID_ARCH_ABI=arm64-v8a \
+  -D CMAKE_ANDROID_STL_TYPE=c++_static \
+  -D ANDROID_USE_LEGACY_TOOLCHAIN_FILE=NO \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D UPDATE_DEPS=ON \
+  -G Ninja
+
+cd build
+ninja
+cd ..
+
+cmake --install build --prefix build/libs/arm64-v8a
+mv build/libs/arm64-v8a/lib/libVkLayer_khronos_validation.so ../../assets/libs/lib/arm64-v8a
+
+cd ../../assets/libs
+zip -r vvl-android.zip lib
+mv vvl-android.zip ../../externals
+cd ../..
+
+
+# Build torzu
 cd src/android
 chmod +x ./gradlew
 ./gradlew "assemble${BUILD_FLAVOR}${BUILD_TYPE}" "bundle${BUILD_FLAVOR}${BUILD_TYPE}"
